e048e2b133236c4501dfe460de141ed59663f7b9
wee5h <69915400+wee5h@users.noreply.github.com>
2020-08-19 12:26:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//hello.java
+//William Lombardo
+//August 19, 2020
+//CMSC 255
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("William");
+		System.out.println("Hello");
+		System.out.println("First lab");
+		System.out.println("August 19");
+		System.out.println("CMSC 255");
+		System.out.println();
+	}
+}
